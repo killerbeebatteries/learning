85bc7211bc1b2eaf69046d72d25db7b5e9eb57b8
killerbeebatteries <somewhere@example.com>
2022-02-03 11:29:59 +0930

    demo of using the readline module

---
+++ js/fcc/nodejs_tutorial/readline.js
@@ -0,0 +1,37 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+										input: process.stdin,
+										output: process.stdout
+							
+									});
+
+let num1 = Math.floor((Math.random() * 10) + 1);
+let num2 = Math.floor((Math.random() * 10) + 1);
+
+let answer = num1 + num2;
+
+rl.question(
+			`What is ${ num1 } + ${ num2 }? \n`,
+			(userInput) => {
+				if(userInput.trim() == answer) {
+					rl.close();
+				} else {
+					rl.setPrompt('Sorry, incorrect. Please try again!\n');
+					rl.prompt();
+					rl.on('line', (userInput) => {
+						if(userInput.trim() == answer) {
+							rl.close();
+						} else {
+							rl.setPrompt(`Sorry, the answer ${userInput} is incorrect. Try again.\n`);
+							rl.prompt();
+						}
+					})
+				}
+			});
+
+rl.on('close', () => {
+		console.log('Correct answer!');
+})
+
+
