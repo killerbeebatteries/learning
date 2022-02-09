88a40183b14f1c747e220370638e75dc711e1b11
killerbeebatteries <somewhere@example.com>
2022-02-09 14:50:32 +0930

    moving our previous app.js

---
+++ js/fcc/nodejs_tutorial/app_prev.js
@@ -0,0 +1,8 @@
+const tutorial = require('./tutorial')
+console.log(tutorial)
+//console.log("Hello world from Nodejs");
+//console.log(tutorial(1,1));
+
+console.log(tutorial.sum(1,1));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathsObject());
