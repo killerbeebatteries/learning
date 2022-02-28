01eb21ad093bea1a7ae68153d15a6acd260d01b2
killerbeebatteries <somewhere@example.com>
2022-02-28 15:45:50 +0930

    broken example of Joi (javascript user input validation library)

--- js/fcc/nodejs_tutorial/app.js
+++ js/fcc/nodejs_tutorial/app.js
@@ -1,6 +1,44 @@
+/*
 // testing a module we downloaded using npm install.
 const _ = require('lodash');
 
 let example =_.fill([1,2,3,4,5], "banana", 1, 4);
 
 console.log(example);
+
+*/
+
+// lets try Express.
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req,res) => {
+	res.send('Hello World');
+});
+
+app.get('/example', (req, res) => {
+	res.send('hitting example route');
+
+});
+
+// route parameters; name and age.
+app.get('/example/:name/:age', (req, res) => {
+	console.log(req.params);
+	console.log(req.query);
+	res.send(req.params.name + ' : ' + req.params.age);
+
+});
+
+// Notes:
+// 	Use route parameters when the parameter is mandatory (must have it for the app to function).
+// 	Use a query string (url?parm1=something&param2=somethingelse) when the parameters are optional.
+
+// example of using static files
+app.use('/public', express.static(path.join(__dirname, 'static')));
+app.get('/another_example', (req, res) => {
+	res.sendFile(path.join(__dirname, 'static', 'index.html'));
+
+});
+
+app.listen(9001);
--- js/fcc/nodejs_tutorial/http_post/app.js
+++ js/fcc/nodejs_tutorial/http_post/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const Joi = require('joi');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -12,6 +13,8 @@ app.get('/', (req, res) => {
 
 });
 
+/*
+// json
 app.post('/', (req, res) => {
 	console.log(req.body);
 	// database work here
@@ -20,6 +23,32 @@ app.post('/', (req, res) => {
 
 	// using json
 	res.json({ success: true});
+
+});
+*/
+
+// joi
+// NOTE: currrently broken, but you get the idea.
+app.post('/', (req, res) => {
+	console.log(req.body);
+	const schema = Joi.object().keys({
+
+		email: Joi.string().trim().email().required(),
+		password: Joi.string().min(5).max(10).required()
+
+	});
+
+	Joi.validate(req.body, schema, (err, result) => {
+
+		if(err) {
+			console.log(err);
+			res.send('an error has occurred.');
+		};
+
+		console.log(result);
+		res.send('successfully posted data.');
+
+	});
 
 });
 
